7f44a586c0523b9682440aac442cbf219da87740
mohit <mohitsingh.it5@gmail.com>
2020-07-24 23:39:44 +0530

    added some comments

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'ui/home.dart';
 import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
 
+/* After adding dependencies run flutter pub get ,restart ide if required
+now manually type the class and propertoes of the downloaded packages after importing as 
+sometimes the copied data shows error, but when typed works fine/
+ */
 main() {
   FlutterStatusbarcolor.setStatusBarColor(Colors.tealAccent);
   runApp(MyBuildContext());
@@ -13,11 +17,3 @@ class MyBuildContext extends StatelessWidget {
     return myApp();
   }
 }
-
-
-/* After adding dependencies run flutter pub get ,
-restart ide if required
-
-now manually type the class and propertoes of the downloaded packages after importing as 
-sometimes the copied data shows error, but when typed works fine/
- */
