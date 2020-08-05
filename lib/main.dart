f9c65436ef302bacf7738552d9b93c572cdc7fed
mohit <mohitsingh.it5@gmail.com>
2020-08-05 13:41:06 +0530

    Remote execution/cgi using flutter http module

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,72 @@
 import 'package:flutter/material.dart';
-import 'ui/home.dart';
+import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
+import 'package:http/http.dart' as http;
 
 main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  /*
+  http module doesn't takes variable as url the url has to be directly given in url field as it gives socket error 
+  */
+  // String date = 'http://192.168.43.55/cgi-bin/date';
+  // String id = "http://192.168.43.55/cgi-bin/id";
+  // String cal = 'http://192.168.43.55/cgi-bin/cal';
+
+  funcdate() async {
+    var response = await http.get('http://192.168.43.55/cgi-bin/date');
+    print("Status code is ${response.statusCode}");
+    print("Output is");
+    print(response.body);
+  }
+
+  funccal() async {
+    var response = await http.get('http://192.168.43.55/cgi-bin/cal');
+    print("Status code is ${response.statusCode}");
+    print("Output is");
+    print(response.body);
+  }
+
+  funcid() async {
+    var response = await http.get('http://192.168.43.55/cgi-bin/id');
+    print("Status code is ${response.statusCode}");
+    print("Output is");
+    print(response.body);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return myapp();
+    FlutterStatusbarcolor.setStatusBarColor(Colors.amber);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.amberAccent,
+        ),
+        body: Container(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              // textDirection: TextDirection.ltr,
+              children: <Widget>[
+                Container(
+                  child: RaisedButton(
+                      child: Text("Execute Date command"), onPressed: funcdate),
+                ),
+                Container(
+                  child: RaisedButton(
+                      child: Text("Execute ID command"), onPressed: funcid),
+                ),
+                Container(
+                  child: RaisedButton(
+                      child: Text("Execute ID command"), onPressed: funccal),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
