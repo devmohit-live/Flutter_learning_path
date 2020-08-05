02ac02509a198cb7e95d6835c83a8c883557f7d1
mohit <mohitsingh.it5@gmail.com>
2020-08-05 23:00:32 +0530

    Flatbutton | TextField | onschange

--- lib/main.dart
+++ lib/main.dart
@@ -2,68 +2,85 @@ import 'package:flutter/material.dart';
 import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
 import 'package:http/http.dart' as http;
 
-main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  var date = "http://192.168.43.55/cgi-bin/date";
-  var id = "http://192.168.43.55/cgi-bin/id";
-  String cal = "http://192.168.43.55/cgi-bin/cal";
-
-  funcdate() async {
-    var response = await http.get(date);
-    print("Status code is ${response.statusCode}");
-    print("Output is");
-    print(response.body);
-  }
-
-  funccal() async {
-    var response = await http.get(cal);
-    print("Status code is ${response.statusCode}");
-    print("Output is");
-    print(response.body);
-  }
-
-  funcid() async {
-    var response = await http.get(id);
-    print("Status code is ${response.statusCode}");
-    print("Output is");
-    print(response.body);
-  }
-
   @override
   Widget build(BuildContext context) {
-    FlutterStatusbarcolor.setStatusBarColor(Colors.amber);
+    var x;
+    var imgurl =
+        "https://scontent.fshl1-1.fna.fbcdn.net/v/t1.0-9/88307149_2759901384127044_8953669088761085952_n.jpg?_nc_cat=108&_nc_sid=85a577&_nc_ohc=1P_NLEQQtKcAX-axSJI&_nc_ht=scontent.fshl1-1.fna&oh=bc555ee56a20e09915b9fc8fd22b27ab&oe=5F4EC29F";
+    funccmd(var cmd) async {
+      String url = "http://192.168.43.55/cgi-bin/$cmd";
+      var response = await http.get(url);
+      print("Status code is ${response.statusCode}");
+      print("Output is");
+      print(response.body);
+      // print(x);
+    }
+
+    FlutterStatusbarcolor.setStatusBarColor(Colors.deepOrange);
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.amberAccent,
-        ),
-        body: Container(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              // textDirection: TextDirection.ltr,
-              children: <Widget>[
-                Container(
-                  child: RaisedButton(
-                      child: Text("Execute Date command"), onPressed: funcdate),
-                ),
-                Container(
-                  child: RaisedButton(
-                      child: Text("Execute ID command"), onPressed: funcid),
-                ),
-                Container(
-                  child: RaisedButton(
-                      child: Text("Execute Cal command"), onPressed: funccal),
-                )
-              ],
-            ),
+          appBar: AppBar(
+            backgroundColor: Colors.deepOrangeAccent,
           ),
-        ),
-      ),
+          body: Center(
+            child: Container(
+              margin: EdgeInsets.all(15),
+              padding: EdgeInsets.symmetric(vertical: 12),
+              width: 300,
+              height: 280,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5.0),
+                color: Colors.deepOrangeAccent,
+              ),
+              child: Column(
+                children: <Widget>[
+                  Image(
+                    alignment: Alignment.center,
+                    image: NetworkImage(imgurl),
+                    fit: BoxFit.contain,
+                    width: 130,
+                    height: 80,
+                  ),
+                  Card(
+                    margin: EdgeInsets.all(10),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        fillColor: Colors.white,
+                        // icon: Icon(Icons.send),
+                        hintText: 'Write Command Here',
+                        // helperText: 'Helper Text',
+                        // counterText: '0 characters',
+                        border: const OutlineInputBorder(),
+                      ),
+                      cursorColor: Colors.green,
+                      cursorWidth: 2,
+                      onChanged: (value) {
+                        x = value;
+                        // print(x);
+                      },
+                    ),
+                  ),
+                  Card(
+                    child: FlatButton(
+                        onPressed: () {
+                          // print(x);
+                          funccmd(x);
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text('Submit'),
+                            Icon(Icons.subdirectory_arrow_left)
+                          ],
+                        )),
+                  )
+                ],
+              ),
+            ),
+          )),
     );
   }
 }
